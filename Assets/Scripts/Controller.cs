d2b000771c9191e806d1c3257f3b0f2e58dd6d84
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2024-11-06 11:25:19 +0400

    Add project files.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 2.5f;
+
+    [SerializeField] private float gravity = 9.81f;
+    
+    [SerializeField] private float mouseSensitivity = 0.3f;
+    [SerializeField] private float verticalRange = 80f;
+
+    private CharacterController characterController;
+    private Camera mainCamera;
+    private PlayerInputHandler inputHandler;
+
+    private Transform targetObject;
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+    [SerializeField] private float interactionDistance = 2.0f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+        inputHandler = PlayerInputHandler.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void LateUpdate()
+    {
+        HandleRotation();
+
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out var hitInfo, interactionDistance) && !hitInfo.collider.gameObject.isStatic) 
+        {
+            targetObject = hitInfo.transform;
+            Debug.Log(targetObject.name + "\t" + Vector3.Distance(targetObject.transform.position, transform.position));
+        }
+    }
+
+    void HandleMovement()
+    {
+        Vector3 inputDirection = new Vector3(inputHandler.MoveInput.x, 0f, inputHandler.MoveInput.y);
+        Vector3 worldDirection  = transform.TransformDirection(inputDirection);
+        worldDirection.Normalize();
+
+        currentMovement.x = worldDirection.x * walkSpeed;
+        currentMovement.z = worldDirection.z * walkSpeed;
+
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f;
+        }
+        else
+        {
+            currentMovement.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void HandleRotation()
+    {
+        float mouseXRotation = inputHandler.LookInput.x * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+        
+        verticalRotation -= inputHandler.LookInput.y * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRange, verticalRange);
+
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public InputActionAsset PlayerControls;
+
+    [SerializeField] private string actionMapName = "Player";
+
+    private string movement = "Movement";
+    private string look = "Look";
+    private string interact = "Interact";
+
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction interactAction;
+
+    public Vector2 MoveInput {  get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool Interact { get ; private set; }
+
+    public static PlayerInputHandler Instance { get; private set; } 
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        moveAction = PlayerControls.FindActionMap(actionMapName).FindAction(movement);
+        lookAction = PlayerControls.FindActionMap(actionMapName).FindAction(look);
+        interactAction = PlayerControls.FindActionMap(actionMapName).FindAction(interact);
+        RegisterInputActions();
+    }
+
+    void RegisterInputActions()
+    {
+        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
+        moveAction.canceled += context => MoveInput = Vector2.zero;
+
+        lookAction.performed += context => LookInput = context.ReadValue<Vector2>();
+        lookAction.canceled += context => LookInput = Vector2.zero;
+
+        interactAction.performed += context => Interact = true;
+        interactAction.canceled += context => Interact = false;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        lookAction.Enable();
+        interactAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        lookAction.Disable();
+        interactAction.Disable();
+    }
+}
